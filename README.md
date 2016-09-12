f2c19ee0d60ae38871fab3bb923c5c22c492d0ab
Walter Zalazar <walter.zalazar@globant.com>
2016-09-12 11:54:03 -0300

    create server for display layouts

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# flexbox
+Flexbox!
+===================
+
+This project is in WIP. The objective is create our layout and share it with the community.
+
+
+----------
+
+
+Run project
+-------------
+
+ 1. Clone repository
+ 2. npm install
+ 3. gulp
+
+ 
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require( 'gulp' );
+var webserver = require('gulp-webserver');
+
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
+
+
+gulp.task('default', ['webserver']);
